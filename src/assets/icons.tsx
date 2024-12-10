647110ad7e9cbddf0071b4750abd55859132538c
Pablo Alves <ribeirojr1522@gmail.com>
2024-12-10 19:19:28 -0300

    feat: :sparkles: add icons

---
+++ src/assets/icons.tsx
@@ -0,0 +1,13 @@
+import { FiLoader } from 'react-icons/fi'
+import { IoMdPhonePortrait } from 'react-icons/io'
+import { FaPlus } from 'react-icons/fa6'
+import { MdModeEditOutline } from 'react-icons/md'
+import { FaTrash } from 'react-icons/fa'
+
+export const iconsApp = {
+  phone: IoMdPhonePortrait,
+  loading: FiLoader,
+  plus: FaPlus,
+  edit: MdModeEditOutline,
+  trash: FaTrash,
+}
