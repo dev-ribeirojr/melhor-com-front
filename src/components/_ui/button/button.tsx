d113a49b0156efca42be3766093bf3ef28c71148
Pablo Alves <ribeirojr1522@gmail.com>
2024-12-10 23:02:27 -0300

    refactor: :recycle: correcting button typing

--- src/components/_ui/button/button.tsx
+++ src/components/_ui/button/button.tsx
@@ -1,8 +1,8 @@
-import { HTMLAttributes } from 'react'
+import { ButtonHTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { iconsApp } from '../../../assets/icons'
 
-interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean
 }
 
@@ -12,7 +12,7 @@ export function Button(data: ButtonProps) {
   return (
     <button
       className={twMerge(
-        'bg-primary-50 px-6 py-2 border border-black rounded-md font-semibold transition-all duration-300 hover:bg-black hover:text-white hover:scale-110 flex gap-1 items-center justify-center',
+        'bg-primary-50 px-6 py-2 border border-black rounded-md transition-all duration-300 hover:bg-black hover:text-white hover:scale-110 flex gap-1 items-center justify-center',
         className,
       )}
       {...rest}
